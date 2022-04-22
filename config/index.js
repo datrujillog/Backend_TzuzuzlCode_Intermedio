dae45094b2efde293fd4ae02b5a79d50094103e1
unknown <diegotrujillo0210@gmail.com>
2022-04-21 20:41:31 -0500

    ok

---
+++ config/index.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+
+const config = {
+    env: process.env.NODE_ENV,
+    port: process.env.PORT,
+}
+
+module.exports = config;
+
---
+++ index.js
@@ -0,0 +1,15 @@
+const {env,port} = require('./config/index');
+
+
+ 
+
+
+
+console.log(port);
+console.log(env);
+
+
+
+
+
+
